557f196ea8049a032fb8b20ecdb1fce520f1f9e4
Krishna Diamesso <krishnadiamesso@gmail.com>
2017-07-23 23:55:21 -0400

    New Version Online Offline with Stream

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
 var app = angular.module('Application', []);
 
 app.controller('myCtrl', function ($scope, $http) {
-    $scope.streamUsers = ["cayinator", "ESL_SC2", "OgamingSC2", "peroniogh", "nl_kripp"];
+    $scope.streamUsers = ["cayinator", "ESL_SC2", "OgamingSC2", "peroniogh", "nl_kripp", "cretetion", "brunofin", "comster404"];
 
-    $scope.streamUsersOff = ["cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
+    $scope.streamUsersOff = ["freecodecamp", "habathcx", "storbeck", "RobotCaleb", "noobs2ninjas"];
 
     $scope.filters = {};
 
@@ -13,45 +13,73 @@ app.controller('myCtrl', function ($scope, $http) {
 
     for (var i = 0; i < $scope.streamUsers.length; i++) {
         $scope.Users = [];
-        $http.get(`https://api.twitch.tv/kraken/streams/${$scope.streamUsers[i]}?client_id=${$scope.clientId}`)
-            .then(function (response) {
-                $scope.User = {
-                    stream: response.data.stream,
-                    streamName: response.data.stream.channel.display_name,
-                    streamUrl: response.data.stream.channel.url,
-                    streamViewers: response.data.stream.viewers,
-                    streamViews: response.data.stream.channel.views,
-                    streamFollowers: response.data.stream.channel.followers,
-                    streamLanguage: response.data.stream.channel.language,
-                    streamGame: response.data.stream.game,
-                    streamLogo: response.data.stream.channel.logo,
-                    streamStatus: response.data.stream.channel.status,
-                    streamOnOff: "Online"
-                }
+        $http.get(`https://api.twitch.tv/kraken/channels/${$scope.streamUsers[i]}?client_id=${$scope.clientId}`)
+            .then(function Success(response) {
+                    if (response.data.stream !== null || undefined) {
+                        $scope.User = {
+                            streamName: response.data.display_name,
+                            streamUrl: response.data.url,
+                            streamViewers: response.data.viewers,
+                            streamViews: response.data.views,
+                            streamFollowers: response.data.followers,
+                            streamLanguage: response.data.language,
+                            streamGame: response.data.game,
+                            streamLogo: response.data.logo,
+                            streamStatus: response.data.status,
+                            streamOnOff: "Online"
+                        };
+                        $scope.Users.push($scope.User);
+                    }
+                },
+                function Error(response ) {
+                    $scope.statusText = response.statusText;
+                    $scope.statusCode = response.status;
+                    $scope.User = {
+                        streamStatus: response.data.message,
+                        //streamName: "$scope.streamUsers[i]",
+                        // streamUrl: ,
+                        // streamViewers: "N/A",
+                        // streamViews: "N/A",
+                        // streamFollowers: "N/A",
+                        // streamLanguage: "N/A",
+                        // streamGame: "N/A",
+                        // streamLogo: "N/A",
+                        // streamOnOff: "N/A",
+                    }
+                    // console.log($scope.statusText);
+                    // console.log($scope.statusCode);
+                    // console.log(3);
+                    $scope.Users.push($scope.User);
+                    //console.log($scope.User);
+                });
 
-                $scope.Users.push($scope.User);
-                console.log($scope.User);
-            });
+        // $http.get(`https://wind-bow.glitch.me/twitch-api/channels/${$scope.streamUsersOff[i]}`)
+        //     .then(function (response) {
+        //             if (response.data.display_name !== '') {
+        //                 $scope.User = {
+        //                     streamName: response.data.display_name,
+        //                     streamUrl: response.data.url,
+        //                     streamViewers: "N/A",
+        //                     streamViews: "N/A",
+        //                     streamFollowers: "N/A",
+        //                     streamLanguage: "N/A",
+        //                     streamGame: "N/A",
+        //                     streamLogo: response.data.logo,
+        //                     streamOnOff: "Offline",
+        //                     streamStatus: response.data.display_name
+        //                 }
 
-        $http.get(`https://wind-bow.glitch.me/twitch-api/users/${$scope.streamUsersOff[i]}`)
-            .then(function (response) {
-                $scope.User = {
-                    stream: "N/A",
-                    streamName: response.data.display_name,
-                    streamUrl: "N/A",
-                    streamViewers: "N/A",
-                    streamViews: "N/A",
-                    streamFollowers: "N/A",
-                    streamLanguage: "N/A",
-                    streamGame: "N/A",
-                    streamLogo: response.data.logo,
-                    streamOnOff: "Offline",
-                    streamStatus: response.data.display_name
-                }
-
-                $scope.Users.push($scope.User);
-                console.log($scope.User);
-            });
+        //                 $scope.Users.push($scope.User);
+        //                 console.log($scope.User);
+        //             }
+        //         },
+        //         function Error(response) {
+        //             $scope.statusText = response.statusText;
+        //             $scope.statusCode = response.status;
+        //              console.log($scope.statusText);
+        //              console.log($scope.statusCode);
+        //              console.log(4);
+        //         });
     }
-    console.log($scope.Users);
+    //console.log($scope.Users);
 });
